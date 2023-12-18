11c8e4a3cf2724750064ecc09f467288259939f2
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-18 08:57:36 +0530

    message: test backend project is working

--- server.js
+++ server.js
@@ -1,28 +1,25 @@
-const express = require("express")
+const express = require("express");
 
-const bodyParser = require('body-parser')
-
+const bodyParser = require("body-parser");
 
 // create a app
-const app = express()
-
+const app = express();
 
 // setup server port
-const port = 7000
+const port = 7000;
 
 // parse requests of content-type - application/x-www-form-urlencoded
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json())
-
-
+app.use(bodyParser.json());
 
 // define a root route
-app.get('/', (req, res) => {
-  res.send("Hello World");
+app.get("/", (req, res) => {
+  res.send("test-backend project is working");
 });
+
 // listen for requests
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
