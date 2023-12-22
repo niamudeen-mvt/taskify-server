cc3113ff8d3b7ff9cac8697ecd9b35b03ba092d9
niamudeen-mvt <niamudeen.mvteams@gmail.com>
2023-12-22 17:13:03 +0530

    message: serving static folder

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const cors = require("cors");
 const router = require("./routes/index.js");
 const connectDb = require("./utils/db.js");
 const colors = require("colors");
-
+const path = require("path");
 const app = express();
+
+app.use(express.static(path.join(__dirname, "uploads")));
 
 app.use(express.json());
 app.use(cors());
